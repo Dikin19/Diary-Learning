776a3159105965ed8f138a1c36cfb5180f41ab45
Dikin19 <muhamadsodikin191919@gmail.com>
2025-06-01 01:53:04 +0700

    setup tailwindcss

--- app/App.jsx
+++ app/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from '../app/assets/react.svg'
-import viteLogo from '../public/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let data = [
+    {
+      nama: "dikin",
+      age: 2,
+      hoby: "sepak bola"
+    },
+    {
+      nama: "sodikin",
+      age: 2,
+      hoby: "sepak bola"
+    },
+    {
+      nama: "Muhamad",
+      age: 2,
+      hoby: "sepak bola"
+    },
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container mx-auto p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+      {data.map((el, index) => (
+        <div
+          key={index}
+          className="bg-white border border-gray-200 rounded-xl overflow-hidden shadow-sm hover:shadow-lg transition duration-300"
+        >
+          <img
+            src={
+              el.gambar ||
+              "https://as2.ftcdn.net/v2/jpg/00/77/91/73/1000_F_77917361_EaOTLo6USgDgyuVFcYVqGCkhZJiZFPvK.jpg"
+            }
+            alt={el.nama}
+            className="h-40 object-cover"
+          />
+          <div>
+            <p>{el.nama}</p>
+            <p>{el.hoby}</p>
+            <p>{el.age}</p>
+          </div>
+        </div>
+
+      ))}
+    </div>
+
+
+  );
+
 }
 
 export default App
